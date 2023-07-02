1376f113a4869bd1d10d4b0aa76c96e0ecb186d7
venkatesh kumar <venkateshkumar1405@gmail.com>
2023-07-03 01:13:40 +0530

    removed package-lock json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const server = require('http').Server(app);
 const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
